8734f632096cdf77c3d588b2b52d24b5f7d43b1f
Shivanimeena <202152340@iiitvadodara.ac.in>
2025-08-07 04:16:49 +0530

    Initial commit

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,28 +1,17 @@
-
-import { Component,  } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { Router } from '@angular/router';
-
-
-
-
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  standalone: true,  // Required for standalone components
+  imports: [RouterModule],  // Enables routerLink in template
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
-  
-   constructor(private router: Router) {}
+  constructor(private router: Router) {}  // Correct injection
 
   navigateTo(page: string) {
     this.router.navigate(['/' + page]);
   }
-
 }
-
-  
-
-  
